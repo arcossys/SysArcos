83fd87193a5832c155906f3a4729a0281660a2e6
Regis Masteguim <regismasteguim@gmail.com>
2020-06-24 20:58:13 -0300

    Edição Volutariar(Regis)

---
+++ SysArcos/SysArcos/formularios/Voluntariar/frmvoluntariar.aspx.designer.cs
@@ -0,0 +1,24 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace SysArcos.formularios.Voluntariar {
+    
+    
+    public partial class frmvoluntariar {
+        
+        /// <summary>
+        /// Controle LblAcao.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblAcao;
+    }
+}
