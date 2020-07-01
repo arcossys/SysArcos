870cfc216bd2489b9f404819aa5fd8704358472e
ronaldomartinsfatec <36782381+ronaldomartinsfatec@users.noreply.github.com>
2020-07-01 19:43:14 -0300

    Atualização do Entityframework devido a adição de dois campos na tabela voluntariar no banco de dados

--- SysArcos/SysArcos/VOLUNTARIAR.cs
+++ SysArcos/SysArcos/VOLUNTARIAR.cs
@@ -16,13 +16,14 @@ namespace SysArcos
     {
         public int ID { get; set; }
         public System.DateTime DATA_INICIAL { get; set; }
-        public System.DateTime DATA_FINAL { get; set; }
+        public Nullable<System.DateTime> DATA_FINAL { get; set; }
         public int ID_VOLUNTARIO { get; set; }
         public int ID_VOLUNTARIADO { get; set; }
         public System.DateTime DATA_HORA_CRIACAO_REGISTRO { get; set; }
         public string DESCRICAO { get; set; }
         public string OBSERVACAO { get; set; }
         public string NOME { get; set; }
+        public string DESCRIÇÃO { get; set; }
     
         public virtual VOLUNTARIADO VOLUNTARIADO { get; set; }
         public virtual VOLUNTARIO VOLUNTARIO { get; set; }
